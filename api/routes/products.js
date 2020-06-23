7362f8ec33f06add310dbe48671a3997bbed16b6
dani.edo <dani.nurafandi@gomodo.tech>
2020-06-24 01:19:29 +0700

    patch (edit) and delete data

--- api/routes/products.js
+++ api/routes/products.js
@@ -16,7 +16,13 @@ router.get("/", (req, res, next) => {
     .exec()
     .then((docs) => {
       console.log("ini seluruh data dar database: " + docs);
+      // if (docs.length > 0) {
       res.status(200).json(docs);
+      // } else {
+      //   res.status(404).json({
+      //     message: "No entries found broo",
+      //   });
+      // }
     })
     .catch((err) => {
       console.log(err);
@@ -80,16 +86,40 @@ router.post("/", (req, res, next) => {
 
 router.patch("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  res.status(200).json({
-    message: "UPDATED products with id " + id,
-  });
+  // res.status(200).json({
+  //   message: "UPDATED products with id " + id,
+  // });
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.delete("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  res.status(200).json({
-    message: "DELETED products with id " + id,
-  });
+  Product.remove({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
